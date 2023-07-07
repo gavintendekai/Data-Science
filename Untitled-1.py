37e8720dd4bea1225296223472085266c39e781e
gavintendekai <127592083+gavintendekai@users.noreply.github.com>
2023-07-07 06:31:27 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+#colors = ["red", "blue", "orange", "white", "green"]
+#For loop iteration
+    #for i in range(0,5):
+        #colors[i]="magenta"
+
+    #print(colors)
+
+#you can use enumarate to iterate values in a list
+    #for i,color in enumerate(colors):
+    #    colors[i]="magenta"
+    #    print(colors[i])
+
+print(1,5)
+
+for i in range(1,5):
+    if (i != 2 ):
+        print(i)
